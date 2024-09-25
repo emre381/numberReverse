0c2e2006e895883f40eb15e1e574b563ea5f3dde
emre381 <cihatemre381@gmail.com>
2024-09-25 23:14:34 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int sayinintersi=0;
+            Console.WriteLine("sayı giriniz:");
+            sayi=Convert.ToInt32(Console.ReadLine());
+            int orjisayi = sayi;
+            do
+            {
+                sayinintersi = sayinintersi * 10;
+                sayinintersi = sayinintersi + sayi % 10;
+                sayi = sayi / 10;
+
+            } while (sayi != 0);
+            Console.WriteLine(orjisayi+"sayısının tersi =" + sayinintersi);
+            Console.ReadLine(); 
+        }
+    }
+}
